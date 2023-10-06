f676c04e95b66ed16a34ef7b06fa8f6e21615062
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-10-05 20:23:27 -0700

    add in function with if statement for past,present,future

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,21 @@ $(function () {
          console.log(divHour)
         localStorage.setItem(divHour, textValue)
         } )
+
+     $(".time-block").each(function(){
+         console.log(this)
+        var idval = $(this).attr("id")
+        console.log(idval) 
+        var split = idval.split('-')[1]
+        console.log(split) 
+        var currentHour = dayjs().format("H")
+        console.log(currentHour)
+        if( currentHour > split){
+          $(this).addClass("past")
+        }else if(currentHour == split){
+          $(this).addClass("present") 
+        }else if(currentHour < split){
+          $(this).addClass("future")
+            }
+          })
 });
